fab354e7db401b7dc248438b2c486b73abc0bbd0
Jarl Tuxen <jart@kea.dk>
2024-10-21 13:52:52 +0200

    Initial commit

---
+++ src/main/java/dk/kea/studentdtostart/StudentDtoStartApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.studentdtostart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDtoStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDtoStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/kea/studentdtostart/StudentDtoStartApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.studentdtostart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDtoStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
