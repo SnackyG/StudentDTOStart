f78f1a902b6612665c328c683186307cfb22b200
Jarl Tuxen <jart@kea.dk>
2024-10-22 09:27:17 +0200

    remove Lombok

--- src/main/java/dk/kea/studentdtostart/model/Student.java
+++ src/main/java/dk/kea/studentdtostart/model/Student.java
@@ -10,11 +10,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Student {
 
     @Id
@@ -36,5 +31,55 @@ public class Student {
         this.bornTime = bornTime;
     }
 
+    public Student(Long id, String name, String password, LocalDate bornDate, LocalTime bornTime) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.bornDate = bornDate;
+        this.bornTime = bornTime;
+    }
+
+    public Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBornDate() {
+        return bornDate;
+    }
+
+    public void setBornDate(LocalDate bornDate) {
+        this.bornDate = bornDate;
+    }
+
+    public LocalTime getBornTime() {
+        return bornTime;
+    }
+
+    public void setBornTime(LocalTime bornTime) {
+        this.bornTime = bornTime;
+    }
 }
 
